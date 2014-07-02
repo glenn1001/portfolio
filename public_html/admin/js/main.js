fd384ac6850d03ec3133f3e0dfc5c9988300aa5c
Glenn Blom <glennblom@gmail.com>
2014-07-02 21:21:21 +0200

    * Add some extra comments for explaining routing in AngularJS

--- public_html/admin/js/main.js
+++ public_html/admin/js/main.js
@@ -11,24 +11,32 @@ portfolioApp.config([
 	'$routeProvider',
 	'$locationProvider',
 	function($routeProvider, $locationProvider) {
-		// 
+		// Set html5 mode of location provider to true (URL's worn't start with hashtag (#) anymore)
 		$locationProvider.html5Mode(true);
 
 		// Routing to correct templates
 		$routeProvider.
 		when('/project', {
+			// Route to template
 			templateUrl: 'views/project.html',
+			// Name of controller (see /admin/js/controllers/project.js)
 			controller: 'ProjectCtrl'
 		}).
 		when('/page', {
+			// Route to template
 			templateUrl: 'views/page.html',
+			// Name of controller (see /admin/js/controllers/page.js)
 			controller: 'PageCtrl'
 		}).
 		when('/social_media', {
+			// Route to template
 			templateUrl: 'views/social_media.html',
+			// Name of controller (see /admin/js/controllers/social_media.js)
 			controller: 'SocialMediaCtrl'
 		}).
+		// If URL doesn't match
 		otherwise({
+			// Redirect to project
 			redirectTo: 'project'
 		});
 	}
