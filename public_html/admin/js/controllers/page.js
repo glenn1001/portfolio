6188e87665d7b0b26de76dd47f6badb42fac967b
Glenn Blom <glennblom@gmail.com>
2014-07-02 18:11:45 +0200

    * Split controllers into multiple files

---
+++ public_html/admin/js/controllers/page.js
@@ -0,0 +1,67 @@
+'use strict';
+
+// Page controller (for viewing all pages, creating new pages, updating and deleting existing pages)
+portfolioApp.controller('PageCtrl', [
+	'$scope',
+	'$route',
+	'Page',
+	function($scope, $route, Page) {
+		// Create new empty page (for form)
+		$scope.page = new Page();
+
+		// Get all pages
+		$scope.pages = Page.query();
+
+		// Reset form so you can create a new page
+		$scope.newPage = function() {
+			$scope.page = new Page();
+			$scope.editing = false;
+		}
+
+		// Edit method for updating a page
+		$scope.editPage = function(page) {
+			$scope.page = page;
+			$scope.editing = true;
+		}
+
+		// Store changes
+		$scope.save = function(page) {
+			// Check if page id is set
+			if ($scope.page.id) {
+				// Page id is set, so update the current page
+				Page.update({_id: $scope.page.id}, $scope.page);
+			} else {
+				// Page id isn't set, so create a new page
+				$scope.page.$save().then(function(response) {
+					// Add response (new page) to the list of current pages
+					$scope.pages.unshift(response)
+				});
+			}
+
+			// Reset editing and form so a new post can be created
+			$scope.editing = false;
+			$scope.page = new Page();
+		}
+
+		// Delete page
+		$scope.delete = function(page) {
+			Page.delete(page)
+			_.remove($scope.pages, page)
+		}
+	}
+]);
+
+// Create resource for page
+portfolioApp.provider('Page', function() {
+	this.$get = ['$resource', function($resource) {
+		// Set location where page can be found
+		var Page = $resource(WS_HOST + 'page/:_id', {}, {
+			// Add update method (isn't supported by default)
+			update: {
+				method: 'PATCH'
+			}
+		});
+
+		return Page;
+	}];
+});
---
+++ public_html/admin/js/controllers/social_media.js
@@ -0,0 +1,67 @@
+'use strict';
+
+// SocialMedia controller (for viewing all socialMedias, creating new socialMedias, updating and deleting existing socialMedias)
+portfolioApp.controller('SocialMediaCtrl', [
+	'$scope',
+	'$route',
+	'SocialMedia',
+	function($scope, $route, SocialMedia) {
+		// Create new empty socialMedia (for form)
+		$scope.socialMedia = new SocialMedia();
+
+		// Get all socialMedias
+		$scope.socialMedias = SocialMedia.query();
+
+		// Reset form so you can create a new socialMedia
+		$scope.newSocialMedia = function() {
+			$scope.socialMedia = new SocialMedia();
+			$scope.editing = false;
+		}
+
+		// Edit method for updating a socialMedia
+		$scope.editSocialMedia = function(socialMedia) {
+			$scope.socialMedia = socialMedia;
+			$scope.editing = true;
+		}
+
+		// Store changes
+		$scope.save = function(socialMedia) {
+			// Check if socialMedia id is set
+			if ($scope.socialMedia.id) {
+				// SocialMedia id is set, so update the current socialMedia
+				SocialMedia.update({_id: $scope.socialMedia.id}, $scope.socialMedia);
+			} else {
+				// SocialMedia id isn't set, so create a new socialMedia
+				$scope.socialMedia.$save().then(function(response) {
+					// Add response (new socialMedia) to the list of current socialMedias
+					$scope.socialMedias.unshift(response)
+				});
+			}
+
+			// Reset editing and form so a new post can be created
+			$scope.editing = false;
+			$scope.socialMedia = new SocialMedia();
+		}
+
+		// Delete socialMedia
+		$scope.delete = function(socialMedia) {
+			SocialMedia.delete(socialMedia)
+			_.remove($scope.socialMedias, socialMedia)
+		}
+	}
+]);
+
+// Create resource for socialMedia
+portfolioApp.provider('SocialMedia', function() {
+	this.$get = ['$resource', function($resource) {
+		// Set location where socialMedia can be found
+		var SocialMedia = $resource(WS_HOST + 'social_media/:_id', {}, {
+			// Add update method (isn't supported by default)
+			update: {
+				method: 'PATCH'
+			}
+		});
+
+		return SocialMedia;
+	}];
+});
